2fadb627240e4a4a0f85315b0b932378523ca305
Tim Ermilov <yamalight@gmail.com>
2014-04-16 14:45:08 +0200

    fix client-side controller generation

--- app/templates/_public/js/controllers.js
+++ app/templates/_public/js/controllers.js
@@ -1,5 +1,5 @@
 module.exports = function applyControllers (app) {
-    app.controller('HomeController', ['$scope', require('./controllers/home.js')]);<% if(this.addAuth) { %>
+    app.controller('HomeController', ['$scope', require('./controllers/home.js')]);<% if(addAuth) { %>
     app.controller('LoginController', ['$scope', require('./controllers/login.js')]);
     app.controller('RegisterController', ['$scope', require('./controllers/register.js')]);<% } %>
 };
