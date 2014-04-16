3f1ad05258b4d0947fe614ed54201c290108f5e7
Tim Ermilov <yamalight@gmail.com>
2014-04-16 14:33:34 +0200

    exit after tests are done

--- app/templates/_gulp/tasks/mocha.js
+++ app/templates/_gulp/tasks/mocha.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
+var exit = require('gulp-exit');
 
 module.exports = function(){
     gulp.src('./tests/**/*.js')
     .pipe(mocha({
         reporter: 'spec',
         timeout: 20000, // 10s
-    }));
+    }))
+    .pipe(exit());
 };
