c9415ae458ef635a93a28262b336c212ab2b9ee5
Tim Ermilov <yamalight@gmail.com>
2014-04-18 13:32:41 +0200

    remove template directive as we now have generator

--- app/templates/_public/js/directives.js
+++ app/templates/_public/js/directives.js
@@ -1,3 +1,2 @@
 module.exports = function applyDirectives (app) {
-    app.directive('customDirective', require('./directives/custom.js'));
 };
--- app/templates/_public/js/directives/custom.js
+++
@@ -1,8 +0,0 @@
-module.exports = function customDirective () {
-    return {
-        restrict: 'A',
-        link: function (scope, elem, attrs) {
-            console.log('customDirective: ', scope, elem, attrs);
-        }
-    };
-};
