1b916141bcf4395245d6cb85dd8d389b557ea5b2
Tim Ermilov <yamalight@gmail.com>
2014-05-09 15:31:54 +0200

    make gulp git work even after git repo is inited

--- app/templates/_gulp/_optional/git.js
+++ app/templates/_gulp/_optional/git.js
@@ -1,17 +1,26 @@
 var gulp = require('gulp');
 var git = require('gulp-git');
 var exit = require('gulp-exit');
+var fs = require('fs');
 
 module.exports = {
     deps: ['bootstrap'],
     work: function() {
-        // init repo
-        git.init({}, function() {
-            // add all files
-            return gulp.src('.')
-                .pipe(git.add())
-                .pipe(git.commit('initial commit'))
-                .pipe(exit());
+        // first check if git repo already exists
+        fs.exists('./.git', function(exists) {
+            // if yes - die
+            if (exists) {
+                return gulp.src('.', {read: false}).pipe(exit());
+            }
+
+            // init repo
+            git.init({}, function() {
+                // add all files
+                return gulp.src('.')
+                    .pipe(git.add())
+                    .pipe(git.commit('initial commit'))
+                    .pipe(exit());
+            });
         });
     }
 };
