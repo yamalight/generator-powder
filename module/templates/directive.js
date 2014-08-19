d0421971df91fbd9ca0b49b5e9199f05bee52690
Tim Ermilov <yamalight@gmail.com>
2014-08-19 10:57:56 +0200

    fix paths generation in module subgenerator

--- module/templates/directive.js
+++ module/templates/directive.js
@@ -1,7 +1,7 @@
 module.exports = function <%= camelizedName %>Directive() {
     return {
         restrict: 'E',
-        templateUrl: '<%= camelizedName %>/directive-template.html',
+        templateUrl: '<%= camelizedNameLower %>/directive-template.html',
         controller: require('./directive-controller'),
     };
 };
