d04c68573e7f01883704eec5322ef9fc7dca2759
Tim Ermilov <yamalight@gmail.com>
2014-10-10 10:40:36 +0200

    add compression and static content caching for 1 day

--- app/templates/server/app/static.js
+++ app/templates/server/app/static.js
@@ -1,12 +1,21 @@
 var browserify = require('browserify-middleware');
 var config = require('../../config');
 var express = require('express');
+var compression = require('compression');
 
+// onde day duration
+var oneDay = 86400000;
+// max age of cache
+var maxAge = oneDay;
+
+// export
 module.exports = function(app, dirname) {
+    // compress all requests
+    app.use(compression());
     // serve main static files
-    app.use(express.static(dirname + '/../client/static'));
+    app.use(express.static(dirname + '/../client/static', {maxAge: maxAge}));
     // serve angular modules static files
-    app.use(express.static(dirname + '/../client/js'));
+    app.use(express.static(dirname + '/../client/js', {maxAge: maxAge}));
     // provide a browserified file at a path
     app.get('/dist/app.min.js', browserify(dirname + '/../client/js/app.js', {
         cache: !config.debug,
